286c7ece081e2790ebe6ae3acd4698a2376f9012
Pedro <pedroscardovelli@gmail.com>
2022-03-04 22:40:56 -0300

    tarefa finalizada

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+ Console.BackgroundColor = ConsoleColor.DarkRed;
+       Console.ForegroundColor = ConsoleColor.White;
+       Console.WriteLine("Você Beberia Ou Não Beberia? - Zé Neto & Cristiano");
+       Console.ResetColor();
+       Console.WriteLine();
+
+       Console.ForegroundColor = ConsoleColor.Green;
+       Console.WriteLine("A culpa é da saudade");
+       Console.WriteLine("Que causa esse sufoco");
+       Console.WriteLine("Tá saindo pelos poros");
+       Console.WriteLine("E me desmanchando aos poucos");
+       Console.ResetColor();
+       Console.WriteLine();
+
+       Console.ForegroundColor = ConsoleColor.DarkYellow;
+       Console.WriteLine("Dói ver ela me esquecendo");
+       Console.WriteLine("Então não venha criticar");
+       Console.WriteLine("A vida que eu tô vivendo");
+       Console.ResetColor();
+       Console.WriteLine();
+
+       Console.ForegroundColor = ConsoleColor.Green;
+       Console.WriteLine("Me diz aí");
+       Console.WriteLine("Se você visse alguém beijando o amor da sua vida");
+       Console.WriteLine("Você beberia ou não beberia?");
+       Console.WriteLine("Você beberia ou não beberia?");
+       Console.ResetColor();
+       Console.WriteLine();
+
+       Console.ForegroundColor = ConsoleColor.DarkYellow;
+       Console.WriteLine("Diz aí");
+       Console.WriteLine("Se você visse alguém beijando o amor da sua vida");
+       Console.WriteLine("Você beberia ou não beberia?");
+       Console.WriteLine("Você beberia");
+       Console.WriteLine("Não vem falar que é fraqueza minha");
+       Console.ResetColor();
+       Console.WriteLine();
+
+       Console.ForegroundColor = ConsoleColor.Green;
+       Console.WriteLine("A culpa é da saudade");
+       Console.WriteLine("Que causa esse sufoco");
+       Console.WriteLine("Tá saindo pelos poros");
+       Console.WriteLine("E me desmanchando aos poucos");
+       Console.ResetColor();
+       Console.WriteLine();
+
+       Console.ForegroundColor = ConsoleColor.DarkYellow;
+       Console.WriteLine("Dói ver ela me esquecendo");
+       Console.WriteLine("Então não venha criticar");
+       Console.WriteLine("A vida que eu tô vivendo");
+       Console.ResetColor();
+       Console.WriteLine();
+
+       Console.ForegroundColor = ConsoleColor.Green;
+       Console.WriteLine("Me diz aí");
+       Console.WriteLine("Se você visse alguém beijando o amor da sua vida");
+       Console.WriteLine("Você beberia ou não beberia?");
+       Console.WriteLine("Você beberia ou não beberia?");
+       Console.ResetColor();
+       Console.WriteLine();
+
+       Console.ForegroundColor = ConsoleColor.DarkYellow;
+       Console.WriteLine("Diz aí");
+       Console.WriteLine("Se você visse alguém beijando o amor da sua vida");
+       Console.WriteLine("Você beberia ou não beberia?");
+       Console.WriteLine("Você beberia");
+       Console.WriteLine("Não vem falar que é fraqueza minha");
+       Console.ResetColor();
+       Console.WriteLine();
